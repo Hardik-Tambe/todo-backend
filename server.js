e2a08d54d12ec063e733dec097a7c591e02510c2
Hardik-Tambe <hardiktambe09@gmail.com>
2024-06-05 10:56:24 +0530

    cors

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
 
-const cors = require('cors')
-const {dbconnect} = require('./utils/db.js')
-const app = express()
-
-const todoRoute = require('./router/todo.js')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { dbconnect } = require('./utils/db.js');
+const todoRoute = require('./router/todo.js');
+const app = express();
 
 const corsOptions = {
-    origin: ["https://react-todo-hardik.netlify.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    origin: "https://react-todo-hardik.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
     optionsSuccessStatus: 200,
-  };
-  
-  app.use(cors(corsOptions));
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use("/todos", todoRoute);
 
 app.use("/",(req,res)=>{
     res.send("Hello")
 })
 
-app.use("/todos",todoRoute)
 
-dbconnect().then(()=>{
-    app.listen(3001,()=>{
-        console.log(`server running at port ${process.env.PORT}`)
-    })
-})
+dbconnect().then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+        console.log(`server running at port ${process.env.PORT || 3001}`);
+    });
+});
